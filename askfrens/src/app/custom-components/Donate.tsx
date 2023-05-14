2682da7439ede2fa0f332531decb270faab63e78
P. Misirov <47452703+misirov@users.noreply.github.com>
2023-05-14 02:45:14 +0200

    donate button metamask tx

--- askfrens/src/app/custom-components/Donate.tsx
+++ askfrens/src/app/custom-components/Donate.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
+import { parseEther } from 'viem'
+
+import { 
+  useSendTransaction,
+  useWaitForTransaction
+ } from 'wagmi'
+
 import "./Donate.css";
+
 
 export default function DonationModal({
   closeModal,
@@ -7,7 +15,7 @@ export default function DonationModal({
   closeModal: () => void;
 }) {
   const [selectedToken, setSelectedToken] = useState<string>("");
-  const [amount, setAmount] = useState<number>(0);
+  const [amount, setAmount] = useState<any>(0);
 
   const handleClickOutside = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -17,6 +25,20 @@ export default function DonationModal({
   const handleClickInside = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
+    
+  const { data, error, isLoading, isError, sendTransaction } = useSendTransaction()
+
+  const handleUserInput = () => {
+    var selected_amount = amount;
+    var selected_token = selectedToken;
+    var selected_address = "0x26D7fDCa6241D1f5B1a92288eb15b14Cf7C0c98d" 
+    
+    sendTransaction({
+      to: selected_address,
+      value: parseEther(selected_amount.toString()),
+    })
+  }
+
 
   return (
     <div className="DonationModal__Overlay" onClick={handleClickOutside}>
@@ -29,7 +51,11 @@ export default function DonationModal({
           <h2>Title</h2>
           <select
             value={selectedToken}
-            onChange={(e) => setSelectedToken(e.target.value)}
+            onChange={(e) => { 
+              setSelectedToken(e.target.value); 
+              //console.log('aa')  
+            } 
+            }
           >
             {/* Add your tokens here */}
             <option value="rrusd">RRUSD</option>
@@ -49,8 +75,9 @@ export default function DonationModal({
           <p>Lens Name: {/* Display lens name here */}</p>
 
           <div className="DonationModal__Buttons">
-            <button>ZkBob Donate</button>
-            <button>SafeFiat Donate</button>
+            <button onClick={handleUserInput}>Donate</button>
+            {/* <button onClick={handleUserInput}> Donate</button>
+            <button>SafeFiat Donate</button> */}
           </div>
         </div>
       </div>
